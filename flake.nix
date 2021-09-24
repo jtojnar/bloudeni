8a2f594eead99f75242d570026e5a060a7ef58aa
Jan Tojnar <jtojnar@gmail.com>
2021-09-24 22:21:53 +0200

    flake: init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Results generator for rogaining";
+
+  inputs = {
+    # Shim to make flake.nix work with stable Nix.
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.csvkit
+            pkgs.python3
+          ];
+        };
+      }
+    );
+}
